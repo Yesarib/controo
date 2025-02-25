6cae8bd0f0733b9c895c246c5b820e5661d4c887
Yesarib <yesaribaris23@gmail.com>
2025-02-25 20:14:09 +0300

    fix: loading components dynamic width

--- components/loading.tsx
+++ components/loading.tsx
@@ -6,12 +6,12 @@ interface LoadingProps {
     isSidebar?: boolean
 }
 
-export default function Loading({ width="200", isSidebar=true }: LoadingProps) {
+export default function Loading({ width = "200", isSidebar = true }: LoadingProps) {
     return (
         <div className="flex items-center space-x-4 mt-2">
-            <div className={`${isSidebar ? "space-y-2":"space-y-8"}`}>
+            <div className={`${isSidebar ? "space-y-2" : "space-y-8"}`}>
                 {[...Array(10)].map((_, index) => (
-                    <Skeleton className={`h-4 w-[${width}px]  bg-gray-800`} key={index} />
+                    <Skeleton style={{ width: `${width}px` }} className="h-4 bg-gray-500" key={index} />
                 ))}
             </div>
         </div>
